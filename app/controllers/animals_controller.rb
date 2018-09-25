b02a4f67e478c0eb94cb8eef24fd2d5d0b793da7
Maryam Shitu <maryamshitu@gmail.com>
2018-09-25 14:12:27 -0700

    Animals#new and Animals#create actions defined, programs accepts and stores new animals

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -5,10 +5,28 @@ class AnimalsController < ApplicationController
 
   def show
     @animal = Animal.find(params[:id].to_i)
-
   end
 
-  def record_not_found
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    animal = Animal.new(animal_params)
+
+    if animal.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def not_found
     render :notfound
+  end
+
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'animals#index'
+
   get '/animals', to: 'animals#index'
 
+  get '/animals/new', to: 'animals#new', as: 'new_animal'
+  post '/animals', to: 'animals#create'
+
   get '/animals/:id', to: 'animals#show', as: 'animal'
+
 end
